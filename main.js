87be568f54bf8b3f4ca49f3be5cd90544406b0fe
amzad hossain <amzad.hossain@baruchmail.cuny.edu>
2016-04-11 17:36:04 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+alert("Hello World!");
+// Variables are
+// assigning value to a character.
+x = 10
+console.log(x)
+//it shoud print 10 on the acreen.
+alert("Print it")
+//comments are for keeping things organized.
+//data types:
+string: ("Hello");
+numbers: 99; 
+
