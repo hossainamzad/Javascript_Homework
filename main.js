67f2c2a780df5ffab9e01eb921af9d00d083858c
amzad hossain <amzad.hossain@baruchmail.cuny.edu>
2016-04-12 12:02:00 -0400

    adding data in the cheatsheet

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ alert("Print it")
 
 ("Hello");
 numbers: 99; // 
+// thses are logical operstors for boolean expressions:
 < > = === !== true, false are booleans
 undefined ()// an empty string is for undefined and meant to be a placeholder.
 
@@ -22,3 +23,19 @@ to access an item in the array:
 //we do :
 console.log [1];
  
+ // i am creating global and local variables and the scopes where they live:
+ var myFavorite = {name: "Rick", background: "Police Officer"};
+ function favoriteCharacter (){
+ 	var myFavorite = {name: "John Snow", background:"soldier"}
+ }
+ // here i declared two variables: one inside the function is local variable, and the one outside the function is global variabe.
+ //once call upom the function, it should console log the John Snow and soldier in the screen.
+ //once i call upon the console.log (myFavorite); it shoud print out Rick and Police officer.
+ 
+
+ //pseudo-code algorithm to check a form's validity:
+  // check if the email box isnt filled with a blank string.
+     // if the email is blank, return the user a value "Invalid" an dthen ask them again to put in a valid inut and keep doing it until a valid enail is provided.
+     //the form shoud have two buttons in it: one for Email and the other one is for password.
+       //allow the users to enter any combination of nubers and letters they want to type in for password. And, the valid email shoud have a @ sign at the end of the email.
+
